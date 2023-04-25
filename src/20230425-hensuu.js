9dd218a6524b7bfef4d4f2420190d41a6d7a82eb
susumu-uchiumi <129709126+susumu-uchiumi@users.noreply.github.com>
2023-04-25 12:26:27 +0900

    prettierを入れた

--- src/20230425-hensuu.js
+++ src/20230425-hensuu.js
@@ -1,6 +1,6 @@
-test('constの練習',() =>{
-const booktitle = "本";
-const bookprice = 3000;
-expect(booktitle).toBe("本");
-expect(bookprice).toBe(3000);
+test("constの練習", () => {
+  const booktitle = "本";
+  const bookprice = 3000;
+  expect(booktitle).toBe("本");
+  expect(bookprice).toBe(3000);
 });
