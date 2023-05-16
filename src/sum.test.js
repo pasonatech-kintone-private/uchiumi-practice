96627f2f50301ec241548d8b2a6171a2329845d7
susumu-uchiumi <129709126+susumu-uchiumi@users.noreply.github.com>
2023-05-16 13:23:06 +0900

    Update sum.test.js

--- src/sum.test.js
+++ src/sum.test.js
@@ -5,3 +5,50 @@ test("adds 1 + 2 to equal 3", () => {
 });
 
 const a = 1;
+
+test("someとfilterの練習", () => {
+
+  const array = [1,2,3,4,5];
+  const array1 = [1,3,5,7];
+  const array2 = [2,4,6,8];
+
+  expect(
+    array.some(
+      (number) => number % 2 === 0
+      )
+    )
+  .toBe(true);
+  expect(
+    array1.some(
+      (number) => number % 2 === 0
+      )
+    )
+  .toBe(false);
+  expect(
+    array2.some(
+      (number) => number % 2 === 0
+      )
+    )
+  .toBe(true);
+
+  expect(
+    array.filter(
+      (number) => number % 2 === 0
+      )
+    )
+  .toStrictEqual([2,4]);
+  expect(
+    array1.filter(
+      (number) => number % 2 === 0
+      )
+    )
+  .toStrictEqual([]);
+  expect(
+    array2.filter(
+      (number) => number % 2 === 0
+      )
+    )
+  .toStrictEqual([2,4,6,8]);
+});
+
+
