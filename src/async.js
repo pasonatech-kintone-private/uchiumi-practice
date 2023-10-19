5fe2c2e55c3278a24e2e0e60e5667e932dc316da
susumu-uchiumi <129709126+susumu-uchiumi@users.noreply.github.com>
2023-10-19 12:20:28 +0900

    並列実行直列実行の練習

--- src/async.js
+++ src/async.js
@@ -25,3 +25,47 @@ asyncPromiseTask()
     // 非同期処理が失敗したときの処理
     console.log(error);
   });
+
+  //1.並列実行
+  const response1 = fetch("http://google.com/");
+  const response2 = fetch("http://www.yahoo.co.jp/");
+  const response3 = fetch("https://kintone.cybozu.co.jp/");
+response1.then((resp)=> {
+    resp.ok
+    console.log("ok1:" + resp.ok);
+}
+);
+response2.then((resp)=> {
+  resp.ok
+  console.log("ok2" + resp.ok);
+}
+);
+response3.then((resp)=> {
+  resp.ok
+  console.log("ok3:" + resp.ok);
+}
+)
+
+ //2.直列実行
+ const response4 = fetch("http://google.com/");
+ const response5 = fetch("http://www.yahoo.co.jp/");
+ const response6 = fetch("https://kintone.cybozu.co.jp/");
+response4.then((resp)=> {
+   resp.ok
+   console.log("ok4:" + resp.ok);
+}
+).then(
+response5.then((resp)=> {
+ resp.ok
+ console.log("ok5" + resp.ok);
+}
+)
+).then(
+response6.then((resp)=> {
+ resp.ok
+ console.log("ok6:" + resp.ok);
+}
+))
+
+
+
---
+++ src/async1.js
@@ -0,0 +1,18 @@
+const response1 = fetch("http://google.com/");
+const response2 = fetch("http://www.yahoo.co.jp/");
+const response3 = fetch("https://kintone.cybozu.co.jp/");
+response1.then((resp)=> {
+  resp.ok
+  console.log("ok1:" + resp.ok);
+}
+);
+response2.then((resp)=> {
+resp.ok
+console.log("ok2" + resp.ok);
+}
+);
+response3.then((resp)=> {
+resp.ok
+console.log("ok3:" + resp.ok);
+}
+)
