563cbdd2b2ff7d8e5f1000ea517698531f033bcf
susumu-uchiumi <129709126+susumu-uchiumi@users.noreply.github.com>
2023-11-02 13:07:13 +0900

    値の評価と表示

--- src/hello.js
+++ src/hello.js
@@ -1,4 +1,4 @@
-console.log("hello world");
+/*console.log("hello world");
 //console.log("test");
 //const testtitle = "テストタイトル";
 let testtitle;
@@ -16,3 +16,13 @@ var x;
 console.log(x);
 var x = "test1";
 console.log(x);
+*/
+const total = 42 + 42; // =>84
+console.log(total);
+
+const value = "値";
+console.log(value); // => ReferenceError: x is not defined
+
+const num = 1;
+//console.log(num+num1); // => ReferenceError: num1 is not defined
+console.log(num + num); // => 2
