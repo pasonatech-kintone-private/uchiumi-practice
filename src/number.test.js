bc9b5b1c616b984d594f0586c56973329ec66ea1
susumu-uchiumi <129709126+susumu-uchiumi@users.noreply.github.com>
2023-12-21 14:49:17 +0900

    Update number.test.js

--- src/number.test.js
+++ src/number.test.js
@@ -5,8 +5,8 @@ test("Number.parseIntの練習", () => {
   const num4 = Number.parseInt("3.14");
   const num5 = Number.parseInt("-2abc");
   const num6 = Number.parseInt("3+5");
-  //const num7 = undefined;
-  //const num8 = Number.parseInt(num7);
+  const num7 = undefined;
+  const num8 = Number.parseInt(num7);
   expect(num1).toBe(123);
   expect(num2).toBe(-10);
   expect(num1 - num2).toBe(133);
@@ -14,7 +14,7 @@ test("Number.parseIntの練習", () => {
   expect(num4).toBe(3);
   expect(num5).toBe(-2);
   expect(num6).toBe(3);
-  //expect(num8).toBe(NaN);
+  expect(num8).toBe(NaN);
 });
 
 test("Number.parseFloatの練習", () => {
