2cf845b95c5e7974b0663047a820b02a1bcee4d5
susumu-uchiumi <129709126+susumu-uchiumi@users.noreply.github.com>
2024-01-11 13:06:21 +0900

    可変長引数の練習

--- src/function.test.js
+++ src/function.test.js
@@ -6,3 +6,16 @@ test("関数の練習", () => {
   expect(callname("田中", "くん")).toBe("田中くん");
   expect(callname("山根", "")).toBe("山根");
 });
+test("関数の練習(可変長引数)", () => {
+  function SUM(...args) {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+      sum = args[i] + sum;
+    }
+
+    return sum;
+  }
+  expect(Math.max(1, 2, 3)).toBe(3);
+  expect(Math.max(1, 4, 3)).toBe(4);
+  expect(SUM(1, 2, 3)).toBe(6);
+});
