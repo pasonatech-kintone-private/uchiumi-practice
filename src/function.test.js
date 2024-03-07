52087646e3e7572f06a8f669ff6de3456a94563c
susumu-uchiumi <129709126+susumu-uchiumi@users.noreply.github.com>
2024-03-07 13:03:17 +0900

    Update function.test.js

--- src/function.test.js
+++ src/function.test.js
@@ -19,3 +19,25 @@ test("関数の練習(可変長引数)", () => {
   expect(Math.max(1, 4, 3)).toBe(4);
   expect(SUM(1, 2, 3)).toBe(6);
 });
+
+test("関数の練習(可変長引数)2", () => {
+  expect(Math.max(1, 2, 3)).toBe(3);
+  expect(Math.max(1, 4, 3)).toBe(4);
+  expect(Math.min(1)).toBe(1);
+  expect(Math.min(2, 5)).toBe(2);
+  expect(Math.min(3, 6, 8)).toBe(3);
+});
+test("関数の練習(分割代入)", () => {
+  function hello({ firstname, familyname }) {
+    return "hello" + firstname + familyname + "san";
+  }
+  expect(hello({ firstname: "Taro", familyname: "Yamada" })).toBe(
+    "helloTaroYamadasan"
+  );
+});
+test("関数の練習(分割代入.配列)", () => {
+  function multiply([x, y]) {
+    return x * y;
+  }
+  expect(multiply([3, 4])).toBe(12);
+});
