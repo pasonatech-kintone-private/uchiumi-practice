e325d61efe4077c1af3dbab45f7d939f21b03794
susumu-uchiumi <129709126+susumu-uchiumi@users.noreply.github.com>
2025-02-13 12:57:28 +0900

    演算子の練習
    AND演算子、OR演算子、NOT演算子
    parseInt、parseFloatの練習

---
+++ src/20250213.test.js
@@ -0,0 +1,36 @@
+test("and演算子", () => {
+    const x = 1;
+    const y = 2;
+    expect((x === 1) && (y === 2)).toBe(true);
+    expect((x === 2) && (y === 2)).toBe(false);
+    expect((x === 1) && (y === 1)).toBe(false);
+    expect((x === 2) && (y === 1)).toBe(false);
+  });
+
+test("or演算子", () => {
+    const x = 1;
+    const y = 2;
+    expect((x === 1) || (y === 2)).toBe(true);
+    expect((x === 2) || (y === 2)).toBe(true);
+    expect((x === 1) || (y === 1)).toBe(true);
+    expect((x === 2) || (y === 1)).toBe(false);
+  });
+
+test("or演算子", () => {
+    const x = 1;
+    const y = 2;
+    expect(!((x === 1) || (y === 2))).toBe(false);
+    expect(!((x === 2) || (y === 2))).toBe(false);
+    expect(!((x === 1) || (y === 1))).toBe(false);
+    expect(!((x === 2) || (y === 1))).toBe(true);
+  });
+
+test("calc", () => {
+    expect((1 + 1) * 2).toBe(4);
+});
+
+test("parsInt", () => {
+    expect(Number.parseInt("17",10)).toBe(17);
+    expect(Number.parseInt(17,10)).toBe(17);
+    expect(Number.parseFloat("-2.8",10)).toBe(-2.8);
+});
